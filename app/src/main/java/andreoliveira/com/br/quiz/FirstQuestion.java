ff5bd4f956e7eab6794e9b3614f0989cfb9c1675
de Oliveira <andre.de.oliveira@accenture.com>
2018-04-12 14:59:26 -0300

    update

--- app/src/main/java/andreoliveira/com/br/quiz/FirstQuestion.java
+++ app/src/main/java/andreoliveira/com/br/quiz/FirstQuestion.java
@@ -3,45 +3,47 @@ package andreoliveira.com.br.quiz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
 public class FirstQuestion extends AppCompatActivity {
 
-    int count;
-    TextView teste;
+    int CountReceived;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_question);
 
-        int countRecebido;
         Intent telaAtual = getIntent();
         Bundle valor = telaAtual.getExtras();
+        CountReceived = valor.getInt("contador");
 
 
-
-        
-        count = valor.getInt("contador") + 1;
-
-        teste = (TextView)findViewById(R.id.test);
-        teste.setText(String.valueOf(count));
-
-
-        Button btnProxima = (Button)findViewById(R.id.btnFirst);
-        btnProxima.setOnClickListener(new View.OnClickListener() {
+        Button btnNext = (Button)findViewById(R.id.btnFirst);
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent proximaPergunta = new Intent(FirstQuestion.this, SecondQuestion.class);
+                proximaPergunta.putExtra("contador2", CountReceived);
                 startActivity(proximaPergunta);
-
-
             }
         });
     }
 }
+
+
+  /*  TextView teste;*/
+
+/*
+
+    RadioGroup rg = findViewById(R.id.radioGroup);
+
+    RadioButton rbutton1 = findViewById(R.id.rb1);
+    RadioButton rbutton2 = findViewById(R.id.rb2);
+    RadioButton rbutton3 = findViewById(R.id.rbCorreta);
+    RadioButton rbutton4 = findViewById(R.id.rb4);
+*/
+
+/* teste = (TextView)findViewById(R.id.test);
+        teste.setText(String.valueOf(CountReceived));*/
--- app/src/main/java/andreoliveira/com/br/quiz/SecondQuestion.java
+++ app/src/main/java/andreoliveira/com/br/quiz/SecondQuestion.java
@@ -5,13 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondQuestion extends AppCompatActivity {
+
+    int count;
+    TextView teste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_question);
+
+        int countRecebido;
+        Intent telaAtual = getIntent();
+        Bundle valor = telaAtual.getExtras();
+
+        teste = (TextView)findViewById(R.id.test);
+        teste.setText(String.valueOf(count));
+        count = valor.getInt("contador2");
+
+        teste = (TextView)findViewById(R.id.test);
+        teste.setText(String.valueOf(count));
 
         Button btnProxima = findViewById(R.id.btnSecond);
 
